99c9358ff4e474a889147df59b03d63cc41691f2
m4thcreator <161872201+m4thcreator@users.noreply.github.com>
2024-03-09 20:18:49 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,48 +2,38 @@ document.addEventListener("DOMContentLoaded", function() {
     const popup = document.querySelector('.popup-overlay');
     const closePopupBtn = document.getElementById('closePopup');
 
-    // Show popup when page loads
     popup.style.display = 'flex';
 
-    // Close popup when close button is clicked
     closePopupBtn.addEventListener('click', function() {
         popup.style.opacity = 0;
         setTimeout(() => {
             popup.style.display = 'none';
             popup.style.opacity = 1;
-        }, 500); // Adjust the duration of the animation (in milliseconds) to match your preference
+        }, 500); 
     });
 });
 
 function searchItems(categoryId) {
-    // Get input value and convert it to uppercase for case-insensitive search
     var input = document.getElementById('searchBar_' + categoryId);
     var filter = input.value.toUpperCase();
 
-    // Get the container holding the items for the specified category
     var container = document.getElementById(categoryId);
 
-    // Get all the images within the category container
     var imgs = container.querySelectorAll('img');
 
-    // Loop through each image
     imgs.forEach(function(img) {
-        // Get the alt text of the current image and convert it to uppercase
         var alt = img.alt.toUpperCase();
         
-        // Get the corresponding item-value span
         var itemValue = img.nextElementSibling;
 
-        // If the alt text contains the search query, display the image and its item-value; otherwise, hide them
         if (alt.indexOf(filter) > -1) {
-            img.style.display = 'inline-flex'; // Show the image
-            // Show the item-value only if the image is being displayed
+            img.style.display = 'inline-flex';
             if (img.style.display !== 'none') {
                 itemValue.style.display = 'inline-flex';
             }
         } else {
-            img.style.display = 'none'; // Hide the image
-            itemValue.style.display = 'none'; // Hide the item-value
+            img.style.display = 'none'; 
+            itemValue.style.display = 'none';
         }
     });
 }
@@ -67,7 +57,7 @@ function clearAllSections() {
         });
     });
     if (!document.querySelector('.toggle-button').classList.contains('active')) {
-        calculateTotalValue(); // Recalculate the total value only if the values are not toggled
+        calculateTotalValue(); 
     }
 }
 
@@ -133,14 +123,11 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 
-    // Add event listeners to search bars to toggle off item values when clicked
     const searchBars = document.querySelectorAll('input[type="text"]');
     searchBars.forEach(searchBar => {
         searchBar.addEventListener('click', function() {
-            // Check if item values are toggled on
             const toggleButton = document.querySelector('.toggle-button');
             if (toggleButton && toggleButton.classList.contains('active')) {
-                // Toggle off item values
                 toggleItemValues();
             }
         });
@@ -157,10 +144,10 @@ document.addEventListener('DOMContentLoaded', function () {
             let totalNumbers = 0;
             
             images.forEach(image => {
-                const value = image.dataset.value; // Get the data-value attribute
-                const number = parseInt(value); // Parse the number from the data-value attribute
-                if (!isNaN(number)) { // Check if the parsed number is a valid number
-                    totalNumbers += number; // Add the number to the total
+                const value = image.dataset.value;
+                const number = parseInt(value);
+                if (!isNaN(number)) { 
+                    totalNumbers += number; 
                 }
             });
             
